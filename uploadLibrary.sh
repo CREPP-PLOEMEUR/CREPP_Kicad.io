518982c07d5724d564e77308ec8bba05713be0e2
Nicolas LE GUERROUE <nicolasleguerroue@gmail.com>
2025-01-01 12:42:30 +0100

    update lib with generation tools

--- uploadLibrary.sh
+++ uploadLibrary.sh
@@ -1,13 +1,4 @@
 #Update repo
-src_lib="CREPP.Components"
-sha256=`sha256sum package.json | cut -d ' ' -f1`
-echo "$sha256"
-size_1=`du -sb $src_lib/footprints | cut -f1`
-echo "$size_1"
-size_2=`du -sb $src_lib/3dmodels | cut -f1`
-echo "$size_2"
-size_3=`du -sb $src_lib/symbols | cut -f1`
-echo "$size_3"
 
 
 JSON_PACKAGE="package.json"
@@ -21,45 +12,36 @@ if [ ! -f package.json ]; then
 fi
 
 TOTAL_PACKAGES=$(jq '.packages | length' "$JSON_PACKAGE")
-
-echo $TOTAL_PACKAGES
+echo -e "Packages found : $TOTAL_PACKAGES"
 
 
 for ((INDEX=0; INDEX<TOTAL_PACKAGES; INDEX++)); do
-    # Extraire l'URL de téléchargement
 
-    if jq -e '.packages' package.json > /dev/null; then
-        echo "Name : "`jq -r '.packages[0].name' package.json`
-    else
-        echo "Not found"
-        
-    fi
     FILE_URL=$(jq -r ".packages[$INDEX].versions[0].download_url" "$JSON_PACKAGE")
-    echo $FILE_URL
-    
-    # Télécharger le fichier temporaire
-    #FILE_NAME="temp_file_$INDEX.zip"
-    #curl -s -o "$FILE_NAME" "$FILE_URL"
-
-
+    echo -n "Computing SHA256 of $FILE_URL"
 
     FILE_NAME="temp_file_$INDEX.zip"
     curl -s -o "$FILE_NAME" "$FILE_URL"
 
-
-    # Calculer le SHA256
     NEW_SHA256=$(sha256sum "$FILE_NAME" | awk '{print $1}')
-    echo $NEW_SHA256
+    echo " : done"
 
-    #Mettre à jour le JSON
-    jq --argjson index "$INDEX" --arg sha256 "$NEW_SHA256" \
-        '.packages[$index].versions[0].download_sha256 = $sha256' \
-        "$JSON_FILE" > temp.json && mv temp.json "$JSON_PACKAGE"
+    #Compressed zip storage
+    size_zip=`du -sb $FILE_NAME | cut -f1`
 
-    # Nettoyer le fichier temporaire
+    unzip temp_file_$INDEX.zip -d tmp_unzip_dir
+    size_unzip=`du -sb tmp_unzip_dir | cut -f1`
+    echo $size_unzip
+    rm -r tmp_unzip_dir
+
+
+    jq --argjson index "$INDEX" --arg sha256 "$NEW_SHA256" --argjson download_size "$size_zip" --argjson install_size "$size_unzip" \
+    '.packages[$index].versions[0].download_sha256 = $sha256 | .packages[$index].versions[0].download_size = $download_size | .packages[$index].versions[0].install_size = $install_size' \
+    "$JSON_PACKAGE" > temp.json 
+    mv temp.json "$JSON_PACKAGE"
+    
     rm "$FILE_NAME"
 
-    echo "SHA256 mis à jour pour l'index $INDEX."
 done
 
 #Update the SHA256 of repository.json
